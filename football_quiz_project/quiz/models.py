24a90403482ebe884812d7b15578885581c580cc
barrakz <b.rakuzy@gmail.com>
2023-11-22 20:03:01 +0100

    Add models: questions, user, result

--- football_quiz_project/quiz/models.py
+++ football_quiz_project/quiz/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    text = models.TextField()
+    correct_answer = models.CharField(max_length=255)
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+
+class Result(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user_answer = models.CharField(max_length=255)
+    is_correct = models.BooleanField()
+
+    
