307139c82e27816a065f192627760108d664923f
barrakz <b.rakuzy@gmail.com>
2023-11-23 21:43:12 +0100

    Adding the is_correct field to the Answer model

--- football_quiz_project/quiz/models.py
+++ football_quiz_project/quiz/models.py
@@ -8,6 +8,7 @@ class Question(models.Model):
 class Answer(models.Model):
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE) # Each answer is associated with a question
     text = models.CharField(max_length=255) # The text of the answer
+    is_correct = models.BooleanField(default=False)  # Indicates whether this answer is correct
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) # Each user has one profile
