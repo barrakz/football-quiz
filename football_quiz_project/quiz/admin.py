d0677d218c581890c3e77f84bc88a77b8f7c88a6
barrakz <b.rakuzy@gmail.com>
2023-11-28 19:40:20 +0100

    update models, text in admin mode

--- football_quiz_project/quiz/admin.py
+++ football_quiz_project/quiz/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Question, Answer, User
+from .models import Question, Answer, UserProfile
 
 admin.site.register(Question)
 admin.site.register(Answer)
-admin.site.register(User)
+admin.site.register(UserProfile)
+
+
 
 
--- football_quiz_project/quiz/models.py
+++ football_quiz_project/quiz/models.py
@@ -5,14 +5,23 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     text = models.TextField() # The text of the question
 
+    def __str__(self):
+        return self.text
+
 class Answer(models.Model):
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE) # Each answer is associated with a question
     text = models.CharField(max_length=255) # The text of the answer
     is_correct = models.BooleanField(default=False)  # Indicates whether this answer is correct
 
+    def __str__(self):
+        return self.text
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) # Each user has one profile
 
+    def __str__(self):
+        return self.user.username 
 
 
     
