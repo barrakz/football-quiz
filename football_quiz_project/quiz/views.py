a0c74bb9f78948e248bd390dc5ae09b086d454cb
barrakz <b.rakuzy@gmail.com>
2023-11-29 20:08:24 +0100

    comment for the quiz_view function

--- football_quiz_project/quiz/views.py
+++ football_quiz_project/quiz/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Question
 
+# View for the quiz - displays a question and its answers.
 def quiz_view(request):
     question = Question.objects.first()
     answers = question.answers.all()
