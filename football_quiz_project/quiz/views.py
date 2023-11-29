566116a140364cead4635eadd97d7b17832391be
barrakz <b.rakuzy@gmail.com>
2023-11-29 20:54:38 +0100

    adding a check for the correct answer to the quiz_view function

--- football_quiz_project/quiz/views.py
+++ football_quiz_project/quiz/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
-from .models import Question
+from .models import Question, Answer
 
 # View for the quiz - displays a question and its answers.
 def quiz_view(request):
     question = Question.objects.first()
     answers = question.answers.all()
+    is_correct = None
+    user_answer = None
 
-    return render(request, 'quiz/question.html', {'question': question, 'answers': answers})
+    if request.method == 'POST':
+        user_answer_id = request.POST.get('answer')
+        user_answer = Answer.objects.get(id=user_answer_id)
+        is_correct = user_answer.is_correct
+
+
+    return render(request, 'quiz/question.html', {
+        'question': question, 
+        'answers': answers,
+        'is_correct': is_correct,
+        'user_answer': user_answer
+    })
 
 
 
