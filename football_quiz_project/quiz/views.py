2e67a6358a79f1878f472e29acc6ad1cd30819e3
barrakz <b.rakuzy@gmail.com>
2023-12-01 20:52:26 +0100

    new quiz_view

--- football_quiz_project/quiz/views.py
+++ football_quiz_project/quiz/views.py
@@ -5,38 +5,28 @@ from django.shortcuts import render, redirect
 from .models import Question, Answer
 
 def quiz_view(request):
-    question = Question.objects.first()
-    answers = question.answers.all()
-    is_correct = None
-    user_answer = None
+    question_index = request.session.get('question_index', 0)
+    questions = Question.objects.prefetch_related('answers').all()
 
-    # Check if the user has already answered
-    if f'answered_{question.id}' in request.session:
-        user_answer_id = request.session[f'answered_{question.id}']
-        user_answer = Answer.objects.get(id=user_answer_id)
-        is_correct = user_answer.is_correct
-        return render(request, 'quiz/question.html', {
-            'question': question, 
-            'answers': answers,
-            'is_correct': is_correct,
-            'user_answer': user_answer,
-            'answered': True  # Add a flag to indicate that the answer has been given
-        })
+    if question_index >= len(questions):
+        # Zakończenie quizu
+        request.session['question_index'] = 0  # Reset indeksu na następny raz
+        return render(request, 'quiz/finish.html')
+
+    question = questions[question_index]
+    is_correct = None
 
     if request.method == 'POST':
-        user_answer_id = request.POST.get('answer')
-        user_answer = Answer.objects.get(id=user_answer_id)
-        is_correct = user_answer.is_correct
-        request.session[f'answered_{question.id}'] = user_answer_id  # Save the answer in the session
-
-        return render(request, 'quiz/question.html', {
-            'question': question, 
-            'answers': answers,
-            'is_correct': is_correct,
-            'user_answer': user_answer
-        })
+        if 'answer' in request.POST:
+            user_answer_id = request.POST.get('answer')
+            user_answer = question.answers.get(id=user_answer_id)
+            is_correct = user_answer.is_correct
+        elif 'next' in request.POST:
+            request.session['question_index'] = question_index + 1
+            return redirect('quiz:quiz_question')
 
     return render(request, 'quiz/question.html', {
-        'question': question, 
-        'answers': answers
+        'question': question,
+        'answers': question.answers.all(),
+        'is_correct': is_correct
     })
